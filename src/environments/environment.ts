700983f98943bb6563e96449569360ff9d656eba
Sitsofe K. Fefeti <skcypher6@gmail.com>
2019-02-21 11:46:42 +0000

    done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyD8krBwwGdk0a6PmRSY1ktoUcff32ZL7cg",
-    authDomain: "gondor-6066b.firebaseapp.com",
-    databaseURL: "https://gondor-6066b.firebaseio.com",
-    projectId: "gondor-6066b",
-    storageBucket: "gondor-6066b.appspot.com",
-    messagingSenderId: "84415301359"
+
   }
 };
 
